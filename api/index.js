330ee871027739349e8eb9c05f142bc8efa4e6f0
whatdaahaael <sujanedwin2006@gmail.com>
2025-06-27 23:47:02 +0530

    Vercel Deployment Fixing

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
-import timetable from './timetable.html';
-
 export default async function handler(req, res) {
   const path = require('path');
+  const fs = require('fs');
   const htmlFile = process.env.HTML_FILE || 'api/timetable.html';
   const filePath = path.join(process.cwd(), htmlFile);
   console.log('Resolved file path:', filePath); // Debug log
   try {
-    const fs = await import('fs/promises');
-    const data = await fs.readFile(filePath, 'utf-8');
+    const data = fs.readFileSync(filePath, 'utf-8');
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send(data);
   } catch (err) {
