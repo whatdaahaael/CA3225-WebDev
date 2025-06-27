50c261aba215c331445ca76a0b40623db1e7222a
whatdaahaael <sujanedwin2006@gmail.com>
2025-06-27 22:54:20 +0530

    Vercel Dep Changes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 export default async function handler(req, res) {
+  const path = require('path');
   const htmlFile = process.env.HTML_FILE || 'all-pages/timetable.html';
+  const filePath = path.join(process.cwd(), htmlFile);
   try {
     const fs = await import('fs/promises');
-    const data = await fs.readFile(htmlFile, 'utf-8');
+    const data = await fs.readFile(filePath, 'utf-8');
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send(data);
   } catch (err) {
